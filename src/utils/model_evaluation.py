0d58541c3ea5164fc0bfbb2926a438304ece5882
Ahmed Abdelrahman <37597383+af-a@users.noreply.github.com>
2025-02-07 12:44:27 +0100

    Add model_evaluation.py util module

---
+++ src/utils/model_evaluation.py
@@ -0,0 +1,88 @@
+
+"""
+Contains general model evaluation functions
+"""
+
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Note: based on plot_trajectories.plot_trajectories()
+def generate_trajectories(ds, reference: np.ndarray, space_stretch: float = 0.1,
+                          n_samples: int = 1000, file_name: str = "test", save_dir: str = "",
+                          n_rollouts: int = 3, rollouts_ic_std: int = 0.1,
+                          show_legends: bool = False, save_rollouts: bool = True):
+    """ Execute a policy for given a DS model and save output trajectories.
+
+    Args:
+        ds (PlanningPolicyInterface): A dynamical system for motion generation task.
+        trajectory (np.ndarray): Input trajectory array (n_samples, dim).
+        space_stretch (float, optional): How much of the entire space to show in vector map.
+            Defaults to 1.
+
+        file_name(str, optional): Name of the plot file. Defaults to "".
+        save_dir(str, optional): Provide a save directory for the figure. Leave empty to
+            skip saving. Defaults to "".
+        n_samples (int, optional): Number of samples in each demonstration. Defaults to 1000.
+        n_rollouts (int, optional): Number of trajectories to reproduce. Defaults to 10.
+        show_legends (bool, optional): Opt to show the legends. Defaults to True.
+    """
+
+    dim = reference.shape[1]
+
+    # Initial and goal states
+    # Note: currently taken directly from training trajectories:
+    goal_point = reference[-1]
+    initial_states = np.array([reference[idx * n_samples] for idx in range(len(reference) // n_samples)])
+    print(f'Initial states ({initial_states.shape}):\n{initial_states}')
+
+    # original policy rollouts
+    dt: float = 0.01
+    lims = [ (reference[:, i].max() - reference[:, i].min()) for i in range(reference.shape[1])]
+    limit = np.linalg.norm(lims) / 100
+
+    simulated_trajs_list = []
+
+    print(f'[INFO] Computed trajectory value limit for generation: {limit}')
+    print(f'[INFO] Generating policy rollouts...')
+    for idx, start in enumerate(initial_states):
+        print(f'[INFO] Generating trajectory for initial state: {start}')
+        simulated_traj: List[np.ndarray] = []
+        simulated_traj.append(np.array([start]).reshape(1, dim))
+
+        distance_to_target = np.linalg.norm(simulated_traj[-1] - goal_point)
+        while distance_to_target > limit and len(simulated_traj) < 5e3:
+            vel = ds.predict(simulated_traj[-1])
+            simulated_traj.append(simulated_traj[-1] + dt * vel)
+            distance_to_target = np.linalg.norm(simulated_traj[-1] - goal_point)
+
+        while len(simulated_traj) < 5e3:
+            if distance_to_target <= limit:
+                print(f'[INFO] Reached goal after {len(simulated_traj)} time steps')
+                break
+            vel = ds.predict(simulated_traj[-1])
+            simulated_traj.append(simulated_traj[-1] + dt * vel)
+            distance_to_target = np.linalg.norm(simulated_traj[-1] - goal_point)
+        else:
+            print(f'[INFO] Failed to reach goal after 5000 timesteps. Terminating...')
+
+        simulated_traj = np.array(simulated_traj)
+        simulated_traj = simulated_traj.reshape(simulated_traj.shape[0],
+                                                simulated_traj.shape[2])
+        simulated_trajs_list.append(simulated_traj)
+
+        if save_rollouts:
+            name = file_name if file_name != "" else 'plot'
+            os.makedirs(os.path.join(save_dir, name), exist_ok=True)
+            np.save(os.path.join(save_dir, name, f'rollout_original_{idx}'), simulated_traj)
+
+    data_dict = {'pos': [traj_array.T.tolist() for traj_array in simulated_trajs_list],
+                 'initial_pos': initial_states.tolist()}
+
+    output_file_path = os.path.join(save_dir, file_name + '.json')
+    print(f'[INFO] Saving generated trajectories in: {output_file_path}')
+    with open(output_file_path, 'w') as file_handle:
+        json.dump(data_dict, file_handle)
+
